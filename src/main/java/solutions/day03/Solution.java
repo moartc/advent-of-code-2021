2049ce2aebfc62ced26f74b6296bec9dab9e5a6c
mo <allenszwester@gmail.com>
2021-12-03 07:49:18 +0100

    Small refactoring. Correct result, so I assume that it works as it should.

--- src/main/java/solutions/day03/Solution.java
+++ src/main/java/solutions/day03/Solution.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Solution {
     public static void main(String[] args) {
@@ -15,16 +16,7 @@ public class Solution {
         }
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < input.get(0).length(); i++) {
-            int zeros = 0;
-            int ones = 0;
-            for (String line : input) {
-                if (line.charAt(i) == '1') {
-                    ones++;
-                } else {
-                    zeros++;
-                }
-            }
-            result.append(ones > zeros ? '1' : '0');
+            result.append(getMoreFrequentCharOnPositionWithDefault1(input, i));
         }
         String result2 = result.toString();
         result2 = result2.replaceAll("0", "x");
@@ -37,26 +29,14 @@ public class Solution {
         List<String> more = new ArrayList<>(input);
         List<String> less = new ArrayList<>(input);
         int stringLength = input.get(0).length();
-        for (int i = 0; i < stringLength; i++) {
-            char toCompare = getMoreFrequentCharOnPosition(more, i);
-            int finalI = i;
-            more.removeIf(s -> s.charAt(finalI) != toCompare);
-            if (more.size() == 1) {
-                break;
-            }
-        }
-        for (int i = 0; i < less.get(0).length(); i++) {
-            int finalI = i;
-            char toCompare = getMoreFrequentCharOnPosition(less, i);
-            less.removeIf(s -> s.charAt(finalI) == toCompare);
-            if (less.size() == 1) {
-                break;
-            }
-        }
+
+        IntStream.range(0, stringLength).forEach(i -> more.removeIf(s -> s.charAt(i) != getMoreFrequentCharOnPositionWithDefault1(more, i) && more.size() > 1));
+        IntStream.range(0, stringLength).forEach(i -> less.removeIf(s -> s.charAt(i) == getMoreFrequentCharOnPositionWithDefault1(less, i) && less.size() > 1));
+
         System.out.println("part2 = " + Integer.valueOf(more.get(0), 2) * Integer.valueOf(less.get(0), 2));
     }
 
-    private static char getMoreFrequentCharOnPosition(List<String> string, int position) {
+    private static char getMoreFrequentCharOnPositionWithDefault1(List<String> string, int position) {
         int zeros = 0;
         int ones = 0;
         for (String line : string) {
