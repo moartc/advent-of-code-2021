a90f0732476316999a08395e36951435d5d25fcd
mo <allenszwester@gmail.com>
2021-12-09 20:08:32 +0100

    Solution for day 9 - refactoring.

--- src/main/java/solutions/day09/Solution.java
+++ src/main/java/solutions/day09/Solution.java
@@ -10,7 +10,6 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
 
-
         URL resource = Solution.class.getResource("/day09.txt");
         List<String> lines = Files.lines(Paths.get(resource.getPath())).toList();
         int[][] locations = lines.stream().map(line -> line.chars().map(c -> c - '0').toArray()).toArray(int[][]::new);
@@ -32,79 +31,50 @@ public class Solution {
         System.out.println("part2 = " + part2);
     }
 
-    static int getBasinSize(int x, int y, int[][] arr) {
+    private static int getBasinSize(int x, int y, int[][] arr) {
         Set<Point> locations = addLowerPoints(x, y, arr, new HashSet<>());
         return locations.size() + 1;
     }
 
-    static Set<Point> addLowerPoints(int x, int y, int[][] arr, Set<Point> locations) {
+    private static Set<Point> addLowerPoints(int x, int y, int[][] arr, Set<Point> locations) {
         int current = arr[y][x];
-        if (y > 0 && y < arr.length - 1) {
-            if (arr[y - 1][x] != 9 && arr[y - 1][x] > current) {
-                locations.add(new Point(y - 1, x));
-                addLowerPoints(x, y - 1, arr, locations);
-            }
-            if (arr[y + 1][x] != 9 && arr[y + 1][x] > current) {
-                locations.add(new Point(y + 1, x));
-                addLowerPoints(x, y + 1, arr, locations);
-            }
-        } else if (y == 0) {
-            int value = arr[y + 1][x];
-            if (value != 9 && value > current) {
-                locations.add(new Point(y + 1, x));
-                addLowerPoints(x, y + 1, arr, locations);
-            }
-        } else if (y == arr.length - 1) {
-            if (arr[y - 1][x] != 9 && arr[y - 1][x] > current) {
-                locations.add(new Point(y - 1, x));
-                addLowerPoints(x, y - 1, arr, locations);
-            }
+        if (y != arr.length - 1) {
+            tryToAddPoints(arr, x, y + 1, current, locations);
         }
-        if (x > 0 && x < arr[0].length - 1) {
-            int value = arr[y][x - 1];
-            if (value != 9 && value > current) {
-                locations.add(new Point(y, x - 1));
-                addLowerPoints(x - 1, y, arr, locations);
-            }
-            if (arr[y][x + 1] != 9 && arr[y][x + 1] > current) {
-                locations.add(new Point(y, x + 1));
-                addLowerPoints(x + 1, y, arr, locations);
-            }
-        } else if (x == 0) {
-            if (arr[y][x + 1] != 9 && arr[y][x + 1] > current) {
-                locations.add(new Point(y, x + 1));
-                addLowerPoints(x + 1, y, arr, locations);
-            }
-        } else if (x == arr[0].length - 1) {
-            if (arr[y][x - 1] != 9 && arr[y][x - 1] > current) {
-                locations.add(new Point(y, x - 1));
-                addLowerPoints(x - 1, y, arr, locations);
-            }
+        if (y <= arr.length - 1 && y != 0) {
+            tryToAddPoints(arr, x, y - 1, current, locations);
+        }
+        if (x != arr[0].length - 1) {
+            tryToAddPoints(arr, x + 1, y, current, locations);
+        }
+        if (x < arr[0].length - 1 && x != 0) {
+            tryToAddPoints(arr, x - 1, y, current, locations);
         }
         return locations;
     }
 
-
-    static boolean isLowestPoint(int x, int y, int[][] arr) {
-        int current = arr[y][x];
-        if (y > 0 && y < arr.length - 1 && (arr[y - 1][x] <= current || arr[y + 1][x] <= current)) {
-            return false;
-        } else if (y == 0 && arr[y + 1][x] <= current) {
-            return false;
-        } else if (y == arr.length - 1 && arr[y - 1][x] <= current) {
-            return false;
+    private static void tryToAddPoints(int[][] arr, int x, int y, int current, Set<Point> locations) {
+        if (arr[y][x] != 9 && arr[y][x] > current) {
+            locations.add(new Point(x, y));
+            addLowerPoints(x, y, arr, locations);
         }
-        if (x > 0 && x < arr[0].length - 1 && (arr[y][x - 1] <= current || arr[y][x + 1] <= current)) {
-            return false;
-        } else if (x == 0 && arr[y][x + 1] <= current) {
-            return false;
-        } else if (x == arr[0].length - 1 && arr[y][x - 1] <= current) {
-            return false;
-        }
-        return true;
     }
 
-    static class Point {
+    private static boolean isLowestPoint(int x, int y, int[][] arr) {
+        int current = arr[y][x];
+        if (y != arr.length - 1 && arr[y + 1][x] <= current) {
+            return false;
+        }
+        if (y <= arr.length - 1 && y != 0 && arr[y - 1][x] <= current) {
+            return false;
+        }
+        if (x != arr[0].length - 1 && arr[y][x + 1] <= current) {
+            return false;
+        }
+        return x > arr[0].length - 1 || x == 0 || arr[y][x - 1] > current;
+    }
+
+    private static class Point {
         int x;
         int y;
 
