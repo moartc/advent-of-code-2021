09cfee78d4a129583e811ac68e012141e0434eb1
szwester <allenszwester@gmail.com>
2021-12-14 12:20:51 +0100

    Solution for day 14 - few changes.

--- src/main/java/solutions/day14/Solution.java
+++ src/main/java/solutions/day14/Solution.java
@@ -4,118 +4,66 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Solution {
 
     public static void main(String[] args) throws IOException {
 
-        boolean test = true;
+        URL resource = Solution.class.getResource("/day14.txt");
+        List<String> lines = Files.lines(Paths.get(resource.getPath())).filter(Predicate.not(String::isEmpty)).toList();
+        String template = lines.get(0);
+        Map<String, Character> pairs = lines.stream().skip(1).map(line -> line.split(" -> ")).collect(Collectors.toMap(s -> s[0], s -> s[1].charAt(0)));
 
-        URL resource = Solution.class.getResource("/day14" + (test ? "_1" : "") + ".txt");
-        List<String> lines = Files.lines(Paths.get(resource.getPath())).toList();
-
-        String template = null;
-        Map<String, Character> pairs = new HashMap<>();
-        int ctr = 0;
-        for (String line : lines) {
-            if (ctr == 0) {
-                template = line;
-            } else if (!line.isEmpty()) {
-                String[] split = line.split(" -> ");
-                pairs.put(split[0], split[1].charAt(0));
-            }
-            ctr++;
-        }
-
-        Map<String, Integer> myMap = new HashMap<>();
-        for(int i = 0; i < template.length()-1; i++) {
-            String key = template.substring(i, i+2);
-            if(!myMap.containsKey(key)) {
-                myMap.put(key, 1);
+        Map<String, Long> startMap = new HashMap<>();
+        for (int i = 0; i < template.length() - 1; i++) {
+            String key = template.substring(i, i + 2);
+            if (!startMap.containsKey(key)) {
+                startMap.put(key, 1L);
             } else {
-                myMap.put(key, myMap.get(key) + 1);
+                startMap.put(key, startMap.get(key) + 1);
             }
         }
-
-        Map<String, Integer> step = step(myMap, pairs);
-        for(int i = 0; i < 11; i++) {
-            myMap = step(myMap, pairs);
+        Map<String, Long> mapPart1 = step(new HashMap<>(startMap), pairs);
+        for (int i = 1; i < 10; i++) {
+            mapPart1 = step(mapPart1, pairs);
         }
+        Map<Character, Long> collectPart1 = mapPart1.entrySet().stream().collect(Collectors.groupingBy(e -> e.getKey().charAt(0), Collectors.summingLong(Map.Entry::getValue)));
+        Long maxPart1 = collectPart1.values().stream().max(Long::compare).get();
+        Long minPart1 = collectPart1.values().stream().min(Long::compare).get();
+        System.out.println("part1 = " + (maxPart1 - minPart1 - 1));
 
-        int h = 0;
-        for(Map.Entry<String, Integer> e : myMap.entrySet()) {
-            if(e.getKey().contains("H")) {
-                h += e.getValue();
-            }
+        Map<String, Long> mapPart2 = step(new HashMap<>(startMap), pairs);
+        for (int i = 1; i < 40; i++) {
+            mapPart2 = step(mapPart2, pairs);
         }
-        System.out.println("h = " + h);
-
-//        System.out.println("part1 = " + (max - min));
-        System.out.println("part2 = ");
+        Map<Character, Long> collectPart2 = mapPart2.entrySet().stream().collect(Collectors.groupingBy(e -> e.getKey().charAt(0), Collectors.summingLong(Map.Entry::getValue)));
+        Long maxPart2 = collectPart2.values().stream().max(Long::compare).get();
+        Long minPart2 = collectPart2.values().stream().min(Long::compare).get();
+        System.out.println("part2 = " + (maxPart2 - minPart2 - 1));
     }
 
-    static Map<String, Integer> step(Map<String, Integer> map, Map<String, Character> mapping) {
-        Map<String, Integer> mapRes = new HashMap<>();
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+    private static Map<String, Long> step(Map<String, Long> map, Map<String, Character> mapping) {
+        Map<String, Long> mapRes = new HashMap<>();
+        for (Map.Entry<String, Long> entry : map.entrySet()) {
             Character s = mapping.get(entry.getKey());
-            String s1 = new String(entry.getKey().charAt(0)+""+s);
-            String s2 = new String(s+""+entry.getKey().charAt(1)+"");
-            if(!mapRes.containsKey(s1)) {
-                mapRes.put(s1, entry.getValue());
-            } else {
-                mapRes.put(s1, mapRes.get(s1) + 1);
-            }
-            if(!mapRes.containsKey(s2)) {
-                mapRes.put(s2, entry.getValue());
-            } else {
-                mapRes.put(s2, mapRes.get(s2) + 1);
-            }
+            String key1 = entry.getKey().charAt(0) + "" + s;
+            String key2 = s + "" + entry.getKey().charAt(1);
+            addKeyToMap(key1, mapRes, entry.getValue());
+            addKeyToMap(key2, mapRes, entry.getValue());
         }
         return mapRes;
     }
 
-    static long getMax(String result) {
-        Map<Character, Long> hm = result.chars().mapToObj(c ->
-                (char) c).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
-        long min = Long.MAX_VALUE;
-        long max = 0;
-        for (Map.Entry<Character, Long> en : hm.entrySet()) {
-            if (en.getValue() > max) {
-                max = en.getValue();
-            }
-            if (en.getValue() < min) {
-                min = en.getValue();
-            }
+    private static void addKeyToMap(String key, Map<String, Long> map, Long value) {
+        if (!map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            map.put(key, map.get(key) + value);
         }
-        return max;
     }
-
-    static long getMin(String result) {
-        Map<Character, Long> hm = result.chars().mapToObj(c ->
-                (char) c).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
-        System.out.println(hm);
-        long min = Long.MAX_VALUE;
-        long max = 0;
-        for (Map.Entry<Character, Long> en : hm.entrySet()) {
-            if (en.getValue() > max) {
-                max = en.getValue();
-            }
-            if (en.getValue() < min) {
-                min = en.getValue();
-            }
-        }
-        return min;
-    }
-
-
-
-
-
 }
-
-
-
-
-
